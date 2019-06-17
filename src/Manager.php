1767d41b9c191eb9ece88b0dcd5266f8092e468d
Puleeno Nguyen <puleeno@gmail.com>
2019-06-17 23:35:12 +0700

    Update structure for assets manager

--- src/Manager.php
+++ src/Manager.php
@@ -3,4 +3,17 @@ namespace Jankx\Asset;
 
 class Manager
 {
+    protected static $instance;
+
+    public static function instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+    }
 }
