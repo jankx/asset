213e082d11e26509ef59b2f3d1d258f7307311a5
Puleeno Nguyen <puleeno@gmail.com>
2020-09-08 00:19:29 +0700

    Fix layout key name

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -68,7 +68,7 @@ if (!class_exists(AssetManager::class)) {
             $defaultAssetCSS = apply_filters('jankx_default_css_resources', array(
                 'jankx-base' => array(
                     'url' => jankx_core_asset_url('css/base.css'),
-                    'version' => '0.0.6',
+                    'version' => '0.0.7',
                 ),
                 'fontawesome' => array(
                     'url' => jankx_core_asset_url('lib/fontawesome/css/all.css'),
