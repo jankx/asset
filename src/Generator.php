4cf2366e87db041afe2b2495e67028940e401b5e
Puleeno Nguyen <puleeno@gmail.com>
2021-05-28 21:28:27 +0700

    Create assets generator prepare caching assets

---
+++ src/Generator.php
@@ -0,0 +1,24 @@
+<?php
+namespace Jankx\Asset;
+
+class Generator
+{
+    protected $globalCss = array();
+    protected $globalJs = array();
+
+    public static function addGlobalCss($css)
+    {
+    }
+
+    public static function addGlobalJs($js)
+    {
+    }
+
+    public static function addCss($css, $type, $object_id = null)
+    {
+    }
+
+    public static function addJs($js, $type, $object_id = null)
+    {
+    }
+}
