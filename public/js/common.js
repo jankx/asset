1d22be16c54a88d7c3622b291f0dfbbd909cb287
Puleeno Nguyen <puleeno@gmail.com>
2021-07-15 16:40:51 +0700

    Support new methods for HTMLElement

--- public/js/common.js
+++ public/js/common.js
@@ -1,28 +1,3 @@
-function jankx_find_element_parent(element, selector) {
-    var e = element, s = selector;
-
-    const parent = e.matches(s);
-    if (parent) {
-        return e;
-    }
-
-    if (e.parentElement) {
-        return jankx_find_element_parent(e.parentElement, s);
-    }
-}
-
-HTMLElement.prototype.findParent = function(selector) {
-    return jankx_find_element_parent(this, selector);
-}
-
-HTMLElement.prototype.parent = function() {
-    return this.parentElement;
-}
-
-HTMLElement.prototype.find = function(selector) {
-    return this.querySelector(selector);
-}
-
 function jankx_create_form_data(body) {
     if (body instanceof FormData) {
         return body;
@@ -65,6 +40,11 @@ function jankx_ajax(url, method = 'GET', body = {}, options = {}, headers = {}) 
     jankx_xhr.onreadystatechange = function () {
         // In local files, status is 0 upon success in Mozilla Firefox
         if(jankx_xhr.readyState === XMLHttpRequest.DONE) {
+            contentType = jankx_xhr.getResponseHeader("Content-Type");
+            if (contentType.indexOf('application/json') > -1) {
+                jankx_xhr.responseJSON = JSON.parse(jankx_xhr.response);
+            }
+
             options.complete(jankx_xhr);
         }
     }
@@ -78,3 +58,63 @@ function jankx_ajax(url, method = 'GET', body = {}, options = {}, headers = {}) 
     return jankx_xhr;
 }
 window.ajax = jankx_ajax;
+
+function jankx_find_element_parent(element, selector) {
+    var e = element, s = selector;
+
+    const parent = e.matches(s);
+    if (parent) {
+        return e;
+    }
+
+    if (e.parentElement) {
+        return jankx_find_element_parent(e.parentElement, s);
+    }
+}
+HTMLElement.prototype.findParent = function(selector) {
+    return jankx_find_element_parent(this, selector);
+}
+
+HTMLElement.prototype.parent = function() {
+    return this.parentElement;
+}
+
+HTMLElement.prototype.find = function(selector) {
+    return this.querySelector(selector);
+}
+
+HTMLElement.prototype.appendHTML = function(html) {
+    this.innerHTML += html;
+}
+HTMLElement.prototype.removeClass = function(clsName) {
+    // For modern browers
+    if (this.classList) {
+        this.classList.remove(clsName);
+    } else {
+        // This case for old IE browser
+        var classes = this.className.split(" ");
+        var i = classes.indexOf(clsName);
+        if (i >= 0) {
+            classes.splice(i, 1);
+            this.className = classes.join(" ");
+        }
+    }
+}
+
+HTMLElement.prototype.addClass = function(clsName) {
+    // For modern browers
+    if (this.classList) {
+        // Add class when the class is not exists
+        if (!this.classList.contains(clsName)) {
+            this.classList.add(clsName);
+        }
+    } else {
+        // This case for old IE browser
+        var classes = this.className.split(" ");
+        var i = classes.indexOf(clsName);
+        if (i < 0 ) {
+            classes.push(clsName);
+            this.className = classes.join(" ");
+        }
+    }
+}
--- src/AssetManager.php
+++ src/AssetManager.php
@@ -6,7 +6,7 @@ use Jankx\Asset\Cache;
 if (!class_exists(AssetManager::class)) {
     class AssetManager
     {
-        const ASSET_LIB_VER = '1.0.0.11';
+        const ASSET_LIB_VER = '1.0.0.18';
 
         protected static $instance;
 
